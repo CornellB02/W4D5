f50bf13f8b5120c9dd032671b563cd3cf3e36bc3
Cornell Bethea <betheacornellb@gmail.com>
2022-12-02 14:50:59 -0500

    phase2done

--- Execution.rb
+++ Execution.rb
@@ -17,44 +17,89 @@
 #O(n^2)
 
 #Phase 2
-def my_min(list)
-    lowest = list[0]
+# def my_min(list)
+#     lowest = list[0]
 
-    list.each {|ele| lowest = ele if ele < lowest }
-    lowest
-end
-#O(n)
+#     list.each {|ele| lowest = ele if ele < lowest }
+#     lowest
+# end
+# #O(n)
 
-# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-# p my_min(list)  # =>  -5
+# # list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# # p my_min(list)  # =>  -5
 require 'byebug'
 
-def largest_contiguous_subsum(list)
+# def largest_contiguous_subsum(list)
   
-  result_arr = []
+#   result_arr = []
 
-  list.each.with_index do |ele, i|
+#   list.each.with_index do |ele, i|
     
-    (0...list.length).each do |j|
-      if j >= i
-      result_arr << list[i..j]
-      end
+#     (0...list.length).each do |j|
+#       if j >= i
+#       result_arr << list[i..j]
+#       end
+#     end
+    
+#   end
+  
+#   sums_of_arr = []
+  
+#   result_arr.each do |sub_arr|
+
+    
+#      sums_of_arr << sub_arr.sum
+    
+
+#   end
+
+#   sums_of_arr.max
+
+# end
+
+
+
+def largest_contiguous_subsum(array)
+    largest_sum = nil
+
+    current_sum = nil
+
+    array.each do |ele|
+        if current_sum.nil?
+            current_sum = ele
+            largest_sum = ele
+            next
+        end
+        largest_sum = current_sum if current_sum > largest_sum 
+        current_sum += ele
+        # debugger
     end
-    
-  end
-  
-  sums_of_arr = []
-  
-  result_arr.each do |sub_arr|
 
-    
-     sums_of_arr << sub_arr.sum
-    
+    current_sum = array.sum
+   
+    array.reverse[0...-1].each do |ele|
+        largest_sum = current_sum if current_sum > largest_sum 
+        current_sum -= ele
+    end
 
-  end
+    current_sum = nil
 
-  sums_of_arr.max
+    array.each do |i|
+         if current_sum.nil?
+            current_sum = i
+            next
+         end
+         largest_sum = current_sum if current_sum > largest_sum 
+        current_sum = i
+    end
 
+    current_sum = array.sum
+
+    array.each do |i|
+        largest_sum = current_sum if current_sum > largest_sum 
+        current_sum -= i
+    end
+    return largest_sum
 end
 
 # Time complexity of O(n)
